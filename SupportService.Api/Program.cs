7ebd41ed34762e826322e1eae8489e27f9b0691e
Lutin <tclutin01@gmail.com>
2023-03-02 20:14:53 +0500

    Initial commit

---
+++ SupportService.Api/Program.cs
@@ -0,0 +1,21 @@
+//return WEbApplicationBuilder
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
+
+//return WEbApplication
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
