aff14a6f1eb6cc565128e3ae0c4db1c84b7bf738
Lutin <tclutin01@gmail.com>
2023-03-02 20:24:33 +0500

    add: DependencyInjection

---
+++ SupportService.Api/src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using SupportService.Api.Services.AuthService;
+
+namespace SupportService.Infrasctructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrasctructure(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ SupportService.Api/src/Services/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using SupportService.Api.Services.AuthService;
+
+namespace SupportService.Services
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
