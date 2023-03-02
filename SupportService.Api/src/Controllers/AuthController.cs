28608013a61ce7b73b64e2e228b9c8657925b969
Lutin <tclutin01@gmail.com>
2023-03-02 20:18:34 +0500

    add: layer controller

---
+++ SupportService.Api/src/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SupportService.Api.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+    }
+}
