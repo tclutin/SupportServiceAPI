49c64f77979279a2c1ad21566b6d1c399b0728c3
Lutin <tclutin01@gmail.com>
2023-03-02 20:28:16 +0500

    fix: add entities layer/fix string

--- SupportService.Api/src/Infrastructure/DependencyInjection.cs
+++ SupportService.Api/src/Infrastructure/DependencyInjection.cs
@@ -2,11 +2,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using SupportService.Api.Services.AuthService;
 
-namespace SupportService.Infrasctructure
+namespace SupportService.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrasctructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             return services;
         }
