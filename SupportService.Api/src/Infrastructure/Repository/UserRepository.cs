5e9a17b5acb533400ab3996d037ee8c68dd7d1d2
Lutin <tclutin01@gmail.com>
2023-03-03 19:16:21 +0500

    add: UserService

---
+++ SupportService.Api/src/Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SupportService.Api.src.Controllers.Dto;
+using SupportService.Api.src.Entities;
+using SupportService.Api.src.Infrastructure.Repository;
+using SupportService.Api.src.Services.UserService;
+
+namespace SupportService.Api.Infrastructure.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly UserDbContext _userDbContext;
+
+        public UserRepository(UserDbContext userDbContext)
+        {
+            _userDbContext = userDbContext;
+        }
+
+        public string Create(User user)
+        {
+            List<User> listOfUser = _userDbContext.User.Where(item => item.Username== user.Username).ToList();
+            if (listOfUser.Count != 0)
+            {
+                return "The user already exists";
+            }
+
+            _userDbContext.User.Add(user);
+            _userDbContext.SaveChanges();
+            return "Successfully";
+        }
+
+        public User? GetUser(AuthUser authUser)
+        {
+            return _userDbContext.User
+                .Where(u => u.Username == authUser.Username && u.Password == authUser.Password)
+                .FirstOrDefault();
+        }
+    }
+}
---
+++ SupportService.Api/src/Services/UserService/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using SupportService.Api.src.Controllers.Dto;
+using SupportService.Api.src.Entities;
+
+namespace SupportService.Api.src.Services.UserService
+{
+    public interface IUserService
+    {
+        public string Register(RegUser regUser);
+        public object Login(AuthUser authUser);
+    }
+}
